725acab459be4542b62852ca4a918aaf6b6b5be2
Myoungsung Ko <msko@bigxdata.io>
2024-09-07 00:46:46 +0900

    init: 미니 주문 API 서버 초기 설정 완료
    - 필요한 의존성 및 기본 설정만 포함된 스프링 부트 프로젝트
    - 멘티들이 클론 후 API 개발을 진행할 수 있도록 기본 구조 세팅

---
+++ src/main/java/com/jasp/miniorderapi/MiniOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.jasp.miniorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniOrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasp/miniorderapi/MiniOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasp.miniorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
